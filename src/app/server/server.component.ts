553a5c99c1b1ecdc0ff60c9c9d32d2e5f14204b4
Martin Andujar <martin@easytech.com.ar>
2018-03-22 17:03:16 -0300

    Added data-binding and string interpolation on the components templates

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-server',
+  selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
 
-  constructor() { }
+  serverId : number = 10;
+  serverStatus : string = 'offline';
 
-  ngOnInit() {
+  getServerStatus() {
+  	return this.serverStatus;
   }
 
+  ngOnInit() {
+
+  }
+  
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  serverCreationStatus = 'No server was created!';
+  allowNewServer = false;
+
+  constructor() { 
+  	setTimeout( () => {
+  		this.allowNewServer = true
+  	}, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+  	this.serverCreationStatus = 'Server was created!';
   }
 
 }
